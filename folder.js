17413e0499492bff6f18931abaae9f51fdd35e74
sashaaladko <74147568+sashaaladko@users.noreply.github.com>
2022-05-20 03:22:12 +0300

    Update folder.js

--- folder.js
+++ folder.js
@@ -1,14 +1,14 @@
-var HIDDEN_CLASS_NAME = 'hidden'
-var TARGET_CLASS_NAME = 'target'
-var SOURCE_CLASS_NAME = 'source'
+const HIDDEN_CLASS_NAME = 'hidden'
+const TARGET_CLASS_NAME = 'target'
+const SOURCE_CLASS_NAME = 'source'
 
 var targetIdToShow = 1
 
 function main() {
-	var targets = getElements(TARGET_CLASS_NAME)
-	var sources = getElements(SOURCE_CLASS_NAME)
+	let targets = getElements(TARGET_CLASS_NAME)
+	let sources = getElements(SOURCE_CLASS_NAME)
 	sources.forEach(function (sourceNode) {
-		var sourceNodeId = extractId(sourceNode, SOURCE_CLASS_NAME)
+		let sourceNodeId = extractId(sourceNode, SOURCE_CLASS_NAME)
 		sourceNode.addEventListener('click', function () {
 			showTarget(targets, sourceNodeId)
 		})
@@ -18,21 +18,21 @@ function main() {
 
 function getElements(type) {
 	return [].slice.call(document.querySelectorAll('.' + type)).sort(function (targetNode1, targetNode2) {
-		var target1Num = extractId(targetNode1, TARGET_CLASS_NAME)
-		var target2Num = extractId(targetNode2, TARGET_CLASS_NAME)
+		let target1Num = extractId(targetNode1, TARGET_CLASS_NAME)
+		let target2Num = extractId(targetNode2, TARGET_CLASS_NAME)
 		return target1Num > target2Num
 	})
 }
 
 function extractId(targetNode, baseClass) {
-	var currentClassIndex = targetNode.classList.length
+	let currentClassIndex = targetNode.classList.length
 	while (currentClassIndex--) {
-		var currentClass = targetNode.classList.item(currentClassIndex)
-		var maybeIdNum = parseInt(currentClass.split('-')[1])
+		let currentClass = targetNode.classList.item(currentClassIndex)
+		let maybeIdNum = parseInt(currentClass.split('-')[1])
 		if (isNaN(maybeIdNum)) {
 			continue
 		}
-		var classStrinToValidate = baseClass + '-' + maybeIdNum
+		let classStrinToValidate = baseClass + '-' + maybeIdNum
 		if (classStrinToValidate === currentClass) {
 			return maybeIdNum
 		}
@@ -41,7 +41,7 @@ function extractId(targetNode, baseClass) {
 
 function showTarget(targets, targetId) {
 	targets.forEach(function (targetNode, targetIndex) {
-    var currentTargetNodeId = extractId(targetNode, TARGET_CLASS_NAME)
+    let currentTargetNodeId = extractId(targetNode, TARGET_CLASS_NAME)
 		if (currentTargetNodeId === targetId) {
 			targetNode.classList.remove(HIDDEN_CLASS_NAME)
 		} else {
